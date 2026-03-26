851d0759f14cb4115917bcbb20877d4d82f5d60a
Kevin Shenk <kevin@avu.nu>
2026-03-26 13:47:04 -0400

    refactor(cockpit): trial disabled allowed-origins

--- flake.nix
+++ flake.nix
@@ -1841,15 +1841,15 @@
               plugins = cfg.cockpit.plugins;
               openFirewall = false; # managed by nftables (LAN/WG already accepted)
               showBanner = cfg.cockpit.showBanner;
-              "allowed-origins" = [
-                "http://${lanGW}"
-                "http://${lanGW}:${toString cfg.cockpit.port}"
-                "http://${cfg.hostName}.local"
-                "http://${cfg.hostName}.local:${toString cfg.cockpit.port}"
-                "http://${cfg.hostName}.${cfg.lan.domain}"
-                "http://${cfg.hostName}.${cfg.lan.domain}:${toString cfg.cockpit.port}"
-              ]
-              ++ cfg.cockpit.allowedOrigins;
+              # "allowed-origins" = [
+              #   "http://${lanGW}"
+              #   "http://${lanGW}:${toString cfg.cockpit.port}"
+              #   "http://${cfg.hostName}.local"
+              #   "http://${cfg.hostName}.local:${toString cfg.cockpit.port}"
+              #   "http://${cfg.hostName}.${cfg.lan.domain}"
+              #   "http://${cfg.hostName}.${cfg.lan.domain}:${toString cfg.cockpit.port}"
+              # ]
+              # ++ cfg.cockpit.allowedOrigins;
               settings = mkMerge [
                 cfg.cockpit.settings
                 {
